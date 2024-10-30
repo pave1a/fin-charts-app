809f68b593738c577ca25377e1c6df59befe8320
Vladyslav Pavelko <vladyslav.pavelko@icloud.com>
2024-10-30 16:21:38 +0200

    Initial commit

---
+++ FinChartsApp/FinChartsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinChartsAppApp.swift
+//  FinChartsApp
+//
+//  Created by Vladyslav Pavelko on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct FinChartsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
